a4cbf645c91cdd34505fd7748911458369dad8a2
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2023-11-04 15:09:07 +0300

    Necessary functions moved to scene class

--- marble-sculp/marble.py
+++ marble-sculp/marble.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 
 from plane import Plane
+from circle import Circle
 
 
 class Marble:
     def __init__(self, pos: list = [0, 0, 0]):
+        self.type = "marble"
         self.vertices = [
             [0, 0, 0],  # A 0
             [1, 0, 0],  # B 1
@@ -16,9 +18,9 @@ class Marble:
             [0, 1, 1],  # H 7
         ]
         self.faces = [
-            [0, 1, 2, 3],  # ABCD
-            [1, 2, 6, 5],  # BCFG
-            [0, 3, 7, 4],  # ADHE
+            [0, 3, 2, 1],  # ADCB
+            [1, 2, 6, 5],  # BCGF
+            [0, 4, 7, 3],  # AEHD
             [4, 5, 6, 7],  # EFGH
             [0, 1, 5, 4],  # ABFE
             [2, 3, 7, 6],  # CDHG
@@ -31,37 +33,10 @@ class Marble:
             self.edges.append([face[3], face[0]])
 
         self.plane = Plane()
+        self.circle = Circle()
 
     def move(self, x: int, y: int, z: int):
         new_vertices = []
         for vertex in self.vertices:
             new_vertices.append([vertex[0] + x, vertex[1] + y, vertex[2] + z])
         self.vertices = new_vertices
-
-    def convert_obj(self):
-        data = ""
-        for vertex in self.vertices:
-            data += f"v {vertex[0]} {vertex[1]} {vertex[2]}\n"
-
-        for plane_vertex in self.plane.vertices:
-            data += f"v {plane_vertex[0]} {plane_vertex[1]} {plane_vertex[2]}\n"
-
-        plane_vertex_start = len(self.vertices)
-        temp_faces = ""
-        for point in self.plane.face:
-            temp_faces += f"{point+plane_vertex_start+1} "
-        data += f"f {temp_faces}\n"
-
-        for face in self.faces:
-            temp_faces = ""
-            for point in face:
-                temp_faces += f"{point+1} "
-            data += f"f {temp_faces}\n"
-
-        with open("./object.obj", "w") as fp:
-            fp.write(data)
-
-
-marb = Marble()
-# marb.move(-0.5, -0.5, -0.5)
-marb.convert_obj()
