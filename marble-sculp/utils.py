67424c19da6c557f8795df50a8b50c7fce262c87
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2023-11-16 17:45:32 +0300

    sort vector indices

--- marble-sculp/utils.py
+++ marble-sculp/utils.py
@@ -10,3 +10,12 @@ def calculate_normal_plane(A: List[int], B: List[int], C: List[int]) -> List[int
     AC = C - A
 
     return np.cross(AB, AC)
+
+
+def sort_points(pts):
+    pts = np.array(pts)
+    centroid = np.sum(pts, axis=0) / pts.shape[0]
+    vector_from_centroid = pts - centroid
+    vector_angle = np.arctan2(vector_from_centroid[:, 1], vector_from_centroid[:, 0])
+    sort_order = np.argsort(vector_angle)
+    return pts[sort_order, :]
