84b3216e41c15568c4e8b5804ff5f009f94900c5
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2023-11-16 17:38:20 +0300

    new object for obj file

---
+++ marble-sculp/disc.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Discontinuity:
+    def __init__(self, vertices: List[List[int]]):
+        self.vertices = vertices
+        self.faces = [[]]
+
+        for ind, value in enumerate(self.vertices):
+            self.faces[0].append(ind)
+
+        print("asdasdsad", self.faces)
