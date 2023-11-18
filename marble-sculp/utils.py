a3c4703cba10fc52a08497876d6e10440b1668ce
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2023-11-19 01:44:58 +0300

    calculate angles from vector

--- marble-sculp/utils.py
+++ marble-sculp/utils.py
@@ -1,5 +1,6 @@
 from typing import List
 import numpy as np
+import random, math
 
 
 def calculate_normal_plane(A: List[int], B: List[int], C: List[int]) -> List[int]:
@@ -19,3 +20,9 @@ def sort_points(pts):
     vector_angle = np.arctan2(vector_from_centroid[:, 1], vector_from_centroid[:, 0])
     sort_order = np.argsort(vector_angle)
     return pts[sort_order, :]
+
+
+def calculate_dip_and_dip_direction_from_unit_vec(unit_vector: List[int]):
+    dip_disc = math.acos(unit_vector[2]) * (180 / math.pi)
+    dip_direction = math.atan2(unit_vector[0], unit_vector[1]) * (180 / math.pi)
+    return [dip_disc / 2, dip_direction / 2]
