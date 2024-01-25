796ba933ad2fce721febbc17ca07895ca546848e
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2024-01-25 23:23:21 +0300

    Tranparency added to all faces

--- marble-sculp/scene.py
+++ marble-sculp/scene.py
@@ -1,6 +1,6 @@
 from marble import Marble
 from circle import Circle
-import json, time, random
+import json, time, random, uuid
 from utils import calculate_dip_and_dip_direction_from_unit_vec
 
 
@@ -11,7 +11,7 @@ class Scene:
     def add(self, _object):
         self.objects.append(_object)
 
-    def convert_obj(self):
+    def convert_obj(self, filename: str) -> str:
         data = "mtllib object.mtl\n"
         index_map = {"vertices": {}, "faces": {}}
         for ind, obj in enumerate(self.objects):
@@ -22,9 +22,9 @@ class Scene:
 
         material = ""
         for ind in range(len(self.objects)):
-            material += f"newmtl Material.{ind}\nNs 360.000000\nKa 1.000000 1.000000 1.000000\nKd {random.randint(0, 1000000)/1000000} {random.randint(0, 1000000)/1000000} {random.randint(0, 1000000)/1000000}\nKs 0.500000 0.500000 0.500000\nKe 0.000000 0.000000 0.000000\nNs 0.000000\nd 1.000000\nillum 2\n\n"
+            material += f"newmtl Material.{ind}\nNs 360.000000\nKa 1.000000 1.000000 1.000000\nKd {random.randint(0, 1000000)/1000000} {random.randint(0, 1000000)/1000000} {random.randint(0, 1000000)/1000000}\nKs 0.500000 0.500000 0.500000\nKe 0.000000 0.000000 0.000000\nNs 0.000000\nd 0.400000\nillum 9\n\n"
 
-        with open("./object.mtl", "w") as fp:
+        with open(f"./static/{filename}.mtl", "w") as fp:
             fp.write(material)
 
         offset = 1
@@ -39,43 +39,44 @@ class Scene:
 
             offset += index_map["vertices"][ind]
 
-        with open("./object.obj", "w") as fp:
+        with open(f"./static/{filename}.obj", "w") as fp:
             fp.write(data)
+        return filename
 
 
-start = time.time()
-scene = Scene()
-marb = Marble()
-marb.move(-0.5, -0.5, -0.5)
-# scene.add(marb)
+# start = time.time()
+# scene = Scene()
+# marb = Marble()
+# marb.move(-0.5, -0.5, -0.5)
+# # scene.add(marb)
 
-circ = Circle()
-circ.move(0, 0, 0)
-# circ.rotate(60, 45)
-# scene.add(circ.intersections(marb.edges, marb.vertices))
-# scene.add(circ)
+# circ = Circle()
+# circ.move(0, 0, 0)
+# # circ.rotate(60, 45)
+# # scene.add(circ.intersections(marb.edges, marb.vertices))
+# # scene.add(circ)
 
-with open("./test.json", "r") as fp:
-    test_data = json.loads(fp.read())
+# with open("./test.json", "r") as fp:
+#     test_data = json.loads(fp.read())
 
-for i in test_data:
-    circ.rotate(i["dip"], i["dip_direction"])
-    disc = circ.intersections(marb.edges, marb.vertices)
-    for _ in range(5):
-        bae = disc.baecher(i["dip"], i["dip_direction"], 3, "log", 5, 4)
-        bae_rot = calculate_dip_and_dip_direction_from_unit_vec(bae["unit_vector"])
-        print(
-            i["dip"],
-            i["dip_direction"],
-            calculate_dip_and_dip_direction_from_unit_vec(disc.normal),
-        )
-        print(bae_rot)
-        print("=====")
-        scene.add(disc)
-        # circ2 = Circle()
-        # circ2.rotate(bae_rot[0], bae_rot[1])
-        # scene.add(circ2)
-    print("*" * 15)
+# for i in test_data:
+#     circ.rotate(i["dip"], i["dip_direction"])
+#     disc = circ.intersections(marb.edges, marb.vertices)
+#     scene.add(disc)
+#     # for _ in range(5):
+#     #     bae = disc.baecher(i["dip"], i["dip_direction"], 3, "log", 5, 4)
+#     #     bae_rot = calculate_dip_and_dip_direction_from_unit_vec(bae["unit_vector"])
+#     #     print(
+#     #         i["dip"],
+#     #         i["dip_direction"],
+#     #         calculate_dip_and_dip_direction_from_unit_vec(disc.normal),
+#     #     )
+#     #     print(bae_rot)
+#     #     print("=====")
+#     #     # circ2 = Circle()
+#     #     # circ2.rotate(bae_rot[0], bae_rot[1])
+#     #     # scene.add(circ2)
+#     print("*" * 15)
 
-scene.convert_obj()
-print("Execution Time:", time.time() - start)
+# scene.convert_obj("asds")
+# print("Execution Time:", time.time() - start)
