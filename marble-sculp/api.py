42f6ad80f889e91654d949541f2d8c0baf61b0e8
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2024-02-07 18:22:46 +0300

    polyhedron structure added to api

--- marble-sculp/api.py
+++ marble-sculp/api.py
@@ -6,12 +6,13 @@ from typing import List
 from typing_extensions import TypedDict
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from scipy.spatial import ConvexHull
+import numpy as np
 
 from scene import Scene
 from marble import Marble
 from circle import Circle
 from utils import calculate_dip_and_dip_direction_from_unit_vec
-from scipy.spatial import ConvexHull
 
 
 class Discontinous(TypedDict):
@@ -140,6 +141,65 @@ async def dfn(request: Request, payload: DiscModel):
         {
             "obj": f"/static/dfn/{payload.filename}.obj",
             "mtl": f"/static/dfn/{payload.filename}.mtl",
+        }
+    )
+
+
+@app.post("/poly")
+async def poly(request: Request, payload: DiscModel):
+    scene = Scene()
+    marb = Marble(
+        size=[payload.sizeX, payload.sizeY, payload.sizeZ],
+        pos=[payload.positionX, payload.positionY, payload.positionZ],
+    )
+    circ = Circle()
+
+    objects = [marb]
+    processed = []
+    for i in payload.data:
+        if [i["dip"], i["dipDirection"]] in processed:
+            continue
+        processed.append([i["dip"], i["dipDirection"]])
+        circ.rotate(i["dip"], i["dipDirection"])
+        for obj in objects:
+            disc = circ.intersections(obj.edges, obj.vertices)
+            if disc is None:
+                temp_objects.append(obj)
+                continue
+
+            left = [d for d in disc.vertices]
+            right = [d for d in disc.vertices]
+
+            for j in obj.vertices:
+                if np.dot(circ.normal, j) < 0:
+                    left.append(j)
+                else:
+                    right.append(j)
+
+            if len(left) > 3:
+                temp_objects.append(
+                    Marble.from_points(
+                        left, ConvexHull(left, qhull_options="QJ Pp").simplices
+                    )
+                )
+            if len(right) > 3:
+                temp_objects.append(
+                    Marble.from_points(
+                        right, ConvexHull(right, qhull_options="QJ Pp").simplices
+                    )
+                )
+        objects = temp_objects
+        temp_objects = []
+
+    for i in objects:
+        scene.add(i)
+
+    scene.convert_obj(filename="poly/" + payload.filename)
+
+    return JSONResponse(
+        {
+            "obj": f"/static/poly/{payload.filename}.obj",
+            "mtl": f"/static/poly/{payload.filename}.mtl",
         }
     )
 
